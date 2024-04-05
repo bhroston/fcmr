8e5d3adb5a434a942dc8bd8699529e25a117e447
benroston <51910844+bhroston@users.noreply.github.com>
2024-04-04 22:10:45 -0500

    test: add tests for get_grey_adj_matrix_from_lower_and_upper_adj_matrices() and ensure they pass

--- tests/testthat/test-fgcmr_utility_functions.R
+++ tests/testthat/test-fgcmr_utility_functions.R
@@ -1,3 +1,33 @@
+test_that("get_grey_adj_matrix_from_lower_and_upper_adj_matrices works", {
+  test_lower_adj_matrix <- data.frame(
+    "C1" = c(0, 0, 0.51, -0.9, 0, 0, 0, 0),
+    "C2" = c(0, 0, 0, 0.7, -0.52, 0, 0, 0),
+    "C3" = c(0.13, 0, 0, 0, 0, 0.3, 0, 0),
+    "C4" = c(0.28, 0.6, 0, 0, 0, 0, 0.2, 0),
+    "C5" = c(0, 0.5, 0, 0, 0, 0, 0, 0),
+    "C6" = c(0, 0, 0, 0, 0, 0, 0, 0.35),
+    "C7" = c(0, 0, 0, -0.16, 0, 0, 0, 0),
+    "C8" = c(0, 0, 0, 0, 0, 0.43, 0, 0)
+  )
+  test_upper_adj_matrix <- data.frame(
+    "C1" = c(0, 0, 1.00, -0.7, 0, 0, 0, 0),
+    "C2" = c(0, 0, 0, 0.9, -0.32, 0, 0, 0),
+    "C3" = c(0.43, 0, 0, 0, 0, 0.5, 0, 0),
+    "C4" = c(0.48, 0.8, 0, 0, 0, 0, 0.4, 0),
+    "C5" = c(0, 0.7, 0, 0, 0, 0, 0, 0),
+    "C6" = c(0, 0, 0, 0, 0, 0, 0, 0.85),
+    "C7" = c(0, 0, 0, 0.34, 0, 0, 0, 0),
+    "C8" = c(0, 0, 0, 0, 0, 0.63, 0, 0)
+  )
+  test_grey_adj_matrix <- get_grey_adj_matrix_from_lower_and_upper_adj_matrices(test_lower_adj_matrix, test_upper_adj_matrix)
+
+  testthat::expect_equal(methods::is(test_grey_adj_matrix), "grey_number")
+  testthat::expect_equal(nrow(test_grey_adj_matrix), 8)
+  testthat::expect_equal(ncol(test_grey_adj_matrix), 8)
+  testthat::expect_equal(test_grey_adj_matrix[[3]][[6]], grey_number(0.3, 0.5))
+})
+
+
 test_that("get_grey_adj_matrix_from_list_of_grey_numbers works", {
   test_adj_matrix_values <- c(
     grey_number(0.5, 0.7),
