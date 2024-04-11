175013c962229ce77727e0c0252f7191835a4d3d
benroston <51910844+bhroston@users.noreply.github.com>
2024-04-10 21:46:47 -0500

    test: add tests for fgcmr
    add tests for fgcmr_simulation and fgcmr. The tests for fgcmr succeed, but getting different results from other researchers on the fgcmr simulation... but I think there's something wrong on their end

---
+++ tests/testthat/test-fgcmr.R
@@ -0,0 +1,131 @@
+
+test_that("fgcmr_simulation works", {
+  # Test from Salmeron & Papageorgiou, 2014 - https://doi.org/10.1007/s10489-013-0511-z
+  # Test 1 (Exact as fcm)
+  w5_2 <- grey_number(0.6, 0.6)
+  w2_5 <- grey_number(-0.42, -0.42)
+  w4_2 <- grey_number(0.8, 0.8)
+  w2_4 <- grey_number(0.7, 0.7)
+  w4_1 <- grey_number(-0.8, -0.8)
+  w1_4 <- grey_number(0.38, 0.38)
+  w3_1 <- grey_number(0.755, 0.755)
+  #w1_3 <- grey_number(0.13, 0.43) They say greyness is 0.2, but can't be
+  #w1_3 <- grey_number(0.33, 0.33)
+  w1_3 <- grey_number(0.28, 0.28) # Might be this?
+  w4_7 <- grey_number(0.09, 0.09)
+  w7_4 <- grey_number(0.3, 0.3)
+  w6_3 <- grey_number(0.4, 0.4)
+  w6_8 <- grey_number(0.53, 0.53)
+  w8_6 <- grey_number(0.6, 0.6)
+
+  test_adj_matrix_edge_weights <- c(w2_5, w5_2, w4_2, w2_4, w4_1, w1_4,
+                                    w3_1, w1_3, w4_7, w7_4, w6_3, w6_8,
+                                    w8_6)
+  test_adj_matrix_edge_indeces <- c("2-5", "5-2", "4-2", "2-4", "4-1", "1-4",
+                                    "3-1", "1-3", "4-7", "7-4", "6-3", "6-8",
+                                    "8-6")
+  test_grey_adj_matrix <- get_grey_adj_matrix_from_list_of_grey_numbers(
+    values = test_adj_matrix_edge_weights,
+    locs = test_adj_matrix_edge_indeces,
+    size = 8
+  )
+
+
+  # Test 2 (Proper grey adjacency matrix)
+  # Test from Salmeron & Papageorgiou, 2014 - https://doi.org/10.1007/s10489-013-0511-z
+  # Does not replicate paper results
+  w5_2 <- grey_number(0.5, 0.7)
+  w2_5 <- grey_number(-0.52, -0.32)
+  w4_2 <- grey_number(0.7, 0.9)
+  w2_4 <- grey_number(0.6, 0.8)
+  w4_1 <- grey_number(-0.9, -0.7)
+  w1_4 <- grey_number(0.28, 0.48)
+  w3_1 <- grey_number(0.51, 1.0)
+  w1_3 <- grey_number(0.13, 0.43)
+  w4_7 <- grey_number(-0.16, 0.34)
+  w7_4 <- grey_number(0.2, 0.4)
+  w6_3 <- grey_number(0.3, 0.5)
+  w6_8 <- grey_number(0.43, 0.63)
+  w8_6 <- grey_number(0.35, 0.85)
+
+  test_adj_matrix_edge_weights <- c(w2_5, w5_2, w4_2, w2_4, w4_1, w1_4,
+                                    w3_1, w1_3, w4_7, w7_4, w6_3, w6_8,
+                                    w8_6)
+  test_adj_matrix_edge_indeces <- c("2-5", "5-2", "4-2", "2-4", "4-1", "1-4",
+                                    "3-1", "1-3", "4-7", "7-4", "6-3", "6-8",
+                                    "8-6")
+  test_grey_adj_matrix <- get_grey_adj_matrix_from_list_of_grey_numbers(
+    values = test_adj_matrix_edge_weights,
+    locs = test_adj_matrix_edge_indeces,
+    size = 8
+  )
+
+  C1 <- grey_number(0.48, 0.48)
+  C2 <- grey_number(0.57, 0.57)
+  C3 <- grey_number(0.58, 0.58)
+  C4 <- grey_number(0.68, 0.68)
+  C5 <- grey_number(0.58, 0.58)
+  C6 <- grey_number(0.59, 0.59)
+  C7 <- grey_number(0.52, 0.52)
+  C8 <- grey_number(0.58, 0.58)
+  test_initial_state_vector = c(C1, C2, C3, C4, C5, C6, C7, C8)
+
+  test_fgcmr_simulation <- simulate_fgcmr(test_grey_adj_matrix, test_initial_state_vector, lambda = 1, activation = "modified-kosko", max_iter = 100, min_error = 1e-8)
+
+  test_steady_states <- test_fgcmr_simulation$state_vectors[nrow(test_fgcmr_simulation$state_vectors),]
+  lapply(test_steady_states, function(x) grey_number(round(x[[1]]$lower, 4), round(x[[1]]$upper, 4)))
+
+  rounded_test_steady_states <- lapply(test_steady_states, function(x) grey_number(round(x[[1]]$lower, 4), round(x[[1]]$upper, 4)))
+
+  # Test 3 (Grey Initial State Vector with Proper grey adjacency matrix)
+  # Test from Salmeron & Papageorgiou, 2014 - https://doi.org/10.1007/s10489-013-0511-z
+  # Does not replicate paper results
+  C1 <- grey_number(0.38, 0.58)
+  C2 <- grey_number(0.47, 0.67)
+  C3 <- grey_number(0.48, 0.68)
+  C4 <- grey_number(0.58, 0.78)
+  C5 <- grey_number(0.48, 0.68)
+  C6 <- grey_number(0.49, 0.69)
+  C7 <- grey_number(0.42, 0.62)
+  C8 <- grey_number(0.48, 0.68)
+  test_initial_state_vector = c(C1, C2, C3, C4, C5, C6, C7, C8)
+
+  test_fgcmr_simulation <- simulate_fgcmr(test_grey_adj_matrix, test_initial_state_vector, lambda = 1, activation = "modified-kosko", max_iter = 100, min_error = 1e-8)
+
+  test_steady_states <- test_fgcmr_simulation$state_vectors[nrow(test_fgcmr_simulation$state_vectors),]
+  lapply(test_steady_states, function(x) grey_number(round(x[[1]]$lower, 4), round(x[[1]]$upper, 4)))
+
+  rounded_test_steady_states <- lapply(test_steady_states, function(x) grey_number(round(x[[1]]$lower, 4), round(x[[1]]$upper, 4)))
+
+
+})
+
+
+test_that("fgcmr works", {
+  # Test from Salmeron & Papageorgiou, 2014 - https://doi.org/10.1007/s10489-013-0511-z
+  w2_5 <- grey_number(0.5, 0.7)
+  w5_2 <- grey_number(-0.52, -0.32)
+  w4_2 <- grey_number(0.7, 0.9)
+  w2_4 <- grey_number(0.6, 0.8)
+  w4_1 <- grey_number(-0.9, -0.7)
+  w1_4 <- grey_number(0.28, 0.48)
+  w3_1 <- grey_number(0.51, 1.0)
+  w1_3 <- grey_number(0.13, 0.43)
+  w4_7 <- grey_number(-0.16, 0.34)
+  w7_4 <- grey_number(0.2, 0.4)
+  w6_3 <- grey_number(0.3, 0.5)
+  w6_8 <- grey_number(0.43, 0.63)
+  w8_6 <- grey_number(0.35, 0.85)
+
+  test_adj_matrix_edge_weights <- c(w2_5, w5_2, w4_2, w2_4, w4_1, w1_4,
+                                    w3_1, w1_3, w4_7, w7_4, w6_3, w6_8,
+                                    w8_6)
+  test_adj_matrix_edge_indeces <- c("2-5", "5-2", "4-2", "2-4", "4-1", "1-4",
+                                    "3-1", "1-3", "4-7", "7-4", "6-3", "6-8",
+                                    "8-6")
+  test_grey_adj_matrix <- get_grey_adj_matrix_from_list_of_grey_numbers(
+    values = test_adj_matrix_edge_weights,
+    locs = test_adj_matrix_edge_indeces,
+    size = 8
+  )
+})
