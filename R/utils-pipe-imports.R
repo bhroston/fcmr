3dfd42edbc5ed954d5099b11c57bb6c0fb198fd7
benroston <51910844+bhroston@users.noreply.github.com>
2024-06-06 19:41:53 -0500

    feat: add %dopar% operator
    add access to the %dopar% operator in package

---
+++ R/utils-pipe-imports.R
@@ -0,0 +1,9 @@
+#' dopar operator
+#'
+#' See https://cran.r-project.org/web/packages/doParallel/vignettes/gettingstartedParallel.pdf for details.
+#'
+#' @name %dopar%
+#' @keywords internal
+#' @export
+#' @importFrom foreach %dopar%
+NULL
