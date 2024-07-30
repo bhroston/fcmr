f70b0512bb954e9bc264e686a450a4a42afca896
benroston <51910844+bhroston@users.noreply.github.com>
2024-07-30 10:31:26 -0500

    del: delete deprecated functions/files/documentation
    This deletes code that has been deprecated by the upcoming major update

--- R/utils-pipe-imports.R
+++
@@ -1,9 +0,0 @@
-#' dopar operator
-#'
-#' See https://cran.r-project.org/web/packages/doParallel/vignettes/gettingstartedParallel.pdf for details.
-#'
-#' @name %dopar%
-#' @keywords internal
-#' @export
-#' @importFrom foreach %dopar%
-NULL
