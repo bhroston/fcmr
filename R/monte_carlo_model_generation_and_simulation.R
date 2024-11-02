e5432a775dc6aeea558c5c3f28a656366d8e0d0c
benroston <51910844+bhroston@users.noreply.github.com>
2024-11-02 08:18:03 -0500

    [docs] minor updates to function docs

--- R/monte_carlo_model_generation_and_simulation.R
+++ R/monte_carlo_model_generation_and_simulation.R
@@ -285,7 +285,8 @@ infer_monte_carlo_fcm_set <- function(mc_adj_matrices = list(matrix()),
 #'
 #' @returns A list of raw bootstrap draws and a dataframe of confidence intervals
 #'
-#' @export man/examples/ex-get_mc_sims_inference_CIs_w_bootstrap.R
+#' @export
+#' @example man/examples/ex-get_mc_sims_inference_CIs_w_bootstrap.R
 get_mc_simulations_inference_CIs_w_bootstrap <- function(mc_simulations_inference_df = data.frame(),
                                                          inference_function = "mean",
                                                          confidence_interval = 0.95,
