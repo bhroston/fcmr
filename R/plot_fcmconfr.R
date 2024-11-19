7f910f63d80d08df09e2cda2f52614c89ccca5d8
benroston <51910844+bhroston@users.noreply.github.com>
2024-11-18 20:12:36 -0600

    [temp] Temporary updates to plot_fcmconfr

--- R/plot_fcmconfr.R
+++ R/plot_fcmconfr.R
@@ -1,21 +1,42 @@
 
 #' Get fcmconfr Object Plot Data
 #' @export
-get_plot_data <- function(fcmconfr_object) {
+get_plot_data <- function(fcmconfr_object, filter_limit = 10e-3) {
 
-  # browser()
+  browser()
 
   fcm_clamping_vector <- fcmconfr_object$params$simulation_opts$clamping_vector
   fcm_nodes <- unique(lapply(fcmconfr_object$params$adj_matrices, colnames))[[1]]
   clamped_nodes <- fcm_nodes[fcm_clamping_vector != 0]
 
+  input_inference_averages <- colSums(fcmconfr_object$inferences$input_fcms$inferences[2:ncol(fcmconfr_object$inferences$input_fcms$inferences)])
+  nodes_of_interest_indexes <- which(input_inference_averages > filter_limit)
+  nodes_of_interest <- names(nodes_of_interest_indexes)
+
+  input_inferences <- as.data.frame(fcmconfr_object$inferences$input_fcms$inferences)
+  aggregate_inferences <- as.data.frame(fcmconfr_object$inferences$aggregate_fcm$inferences)
+  mc_inferences <- as.data.frame(fcmconfr_object$inferences$monte_carlo_fcms$all_inferences)
+
+  input_inferences <- fcmconfr_object$inferences$input_fcms$inferences[, c(1, nodes_of_interest_indexes + 1)] # Add + 1 to match column indexes in input_fcms$inferences dataframe
+  aggregate_inferences <- data.frame(
+    aggregate_inferences[, nodes_of_interest_indexes]
+  )
+  names(aggregate_inferences) <- nodes_of_interest
+
+  mc_inferences <- data.frame(
+    mc_inferences[, nodes_of_interest_indexes]
+  )
+  colnames(mc_inferences) <- nodes_of_interest
+
   if (fcmconfr_object$fcm_class == "conventional") {
     fcm_class_subtitle <- "Conventional FCMs"
-    aggregate_inferences_longer <- tidyr::pivot_longer(fcmconfr_object$inferences$aggregate_fcm$inferences, cols = 1:ncol(fcmconfr_object$inferences$aggregate_fcm$inferences))
+    input_inferences_longer <- tidyr::pivot_longer(input_inferences, cols = 2:ncol(input_inferences))
+    aggregate_inferences_longer <- tidyr::pivot_longer(aggregate_inferences, cols = 1:ncol(aggregate_inferences))
+    mc_inferences_longer <- tidyr::pivot_longer(mc_inferences, cols = 1:ncol(mc_inferences))
 
-    max_y <- max(max(mc_inferences_longer$value), max(aggregate_inferences_longer$value))
+    max_y <- max(max(input_inferences_longer$value), max(mc_inferences_longer$value), max(aggregate_inferences_longer$value))
     max_y <- (ceiling(max_y*1000))/1000
-    min_y <- min(min(mc_inferences_longer$value), min(aggregate_inferences_longer$value))
+    min_y <- min(min(input_inferences_longer$value), min(mc_inferences_longer$value), min(aggregate_inferences_longer$value))
     min_y <- (floor(min_y*1000))/1000
 
   } else if (fcmconfr_object$fcm_class == "ivfn") {
@@ -37,11 +58,10 @@ get_plot_data <- function(fcmconfr_object) {
     fcm_class_subtitle <- "TFN FCM"
   }
 
+  input_inferences_longer$analysis_source <- "input"
   aggregate_inferences_longer$analysis_source <- "aggregate"
   mc_inferences_longer$analysis_source <- "mc_inferences"
   # mc_CIs$analysis_source <- "mc"
-
-
 
   # aggregate_inferences_longer <- tidyr::pivot_longer(fcmconfr_object$inferences$aggregate_fcm$inferences, cols = 1:ncol(fcmconfr_object$inferences$aggregate_fcm$inferences))
 
@@ -71,6 +91,7 @@ get_plot_data <- function(fcmconfr_object) {
   list(
     fcm_class_subtitle = fcm_class_subtitle,
     clamped_nodes,
+    input_inferences = input_inferences_longer,
     aggregate_inferences = aggregate_inferences_longer,
     mc_inferences = mc_inferences_longer,
     # mc_CIs = mc_CIs,
@@ -84,6 +105,7 @@ get_plot_data <- function(fcmconfr_object) {
 #' @export
 autoplot.fcmconfr <- function(fcmconfr_object,
                               coord_flip = TRUE,
+                              inferences_in_plot = c("input", "aggregate", "mc"),
                               aggregate_fill = "#f7e3dd",
                               aggregate_color = "#E1D1CB",
                               aggregate_alpha = 0.7,
@@ -95,39 +117,55 @@ autoplot.fcmconfr <- function(fcmconfr_object,
   browser()
 
   if (fcmconfr_object$fcm_class == "conventional") {
-    ggplot_main <- ggplot() +
-      geom_col(
+    ggplot() +
+      ggplot2::geom_point(
+        data = plot_data$input_inferences,
+        aes(x = .data$name, y = .data$value), alpha = 0.25
+      ) +
+      ggplot2::geom_point(
+        data = plot_data$mc_inferences,
+        aes(x = .data$name, y = .data$value),
+      ) +
+      ggplot2::geom_point(
         data = plot_data$aggregate_inferences,
-        aes(x = .data$name, y = .data$value,
-            fill = .data$analysis_source, color = .data$analysis_source),
-        color = aggregate_color, alpha = aggregate_alpha,
-        width = 0.8
+        aes(x = .data$name, y = .data$value), shape = 17, color = "red", size = 3,
       ) +
-      geom_errorbar(
-        data = plot_data$mc_CIs,
-        aes(x = .data$node, ymin = .data$lower_0.025, ymax = .data$upper_0.975,
-            color = plot_data$mc_CIs$analysis_source),
-        width = 0.5, alpha = 0.7,
-        linewidth = 0.5
-      ) +
-      scale_fill_manual(
-        values = c(mc = mc_fill, aggregate = aggregate_fill),
-        labels = c(mc = "Monte Carlo Average", aggregate = "Aggregate")
-      ) +
-      scale_color_manual(
-        values = c(mc = "black"),
-        labels = c(mc = "Monte Carlo CIs")
-      ) +
-      scale_y_continuous(
-        expand = c(0, 0),
-        limits = c(plot_data$min_activation, plot_data$max_activation)
-      ) +
-      guides(alpha = "none", color = guide_legend(order = 1), shape = guide_legend(order = 2)) +
-      expand_limits(y = 0) +
-      labs(
-        x = "Inference Value",
-        y = "Concept (Node)"
-      )
+      default_theme() +
+      coord_flip()
+
+    # ggplot_main <- ggplot() +
+    #   geom_col(
+    #     data = plot_data$aggregate_inferences,
+    #     aes(x = .data$name, y = .data$value,
+    #         fill = .data$analysis_source, color = .data$analysis_source),
+    #     color = aggregate_color, alpha = aggregate_alpha,
+    #     width = 0.8
+    #   ) +
+    #   geom_errorbar(
+    #     data = plot_data$mc_CIs,
+    #     aes(x = .data$node, ymin = .data$lower_0.025, ymax = .data$upper_0.975,
+    #         color = plot_data$mc_CIs$analysis_source),
+    #     width = 0.5, alpha = 0.7,
+    #     linewidth = 0.5
+    #   ) +
+    #   scale_fill_manual(
+    #     values = c(mc = mc_fill, aggregate = aggregate_fill),
+    #     labels = c(mc = "Monte Carlo Average", aggregate = "Aggregate")
+    #   ) +
+    #   scale_color_manual(
+    #     values = c(mc = "black"),
+    #     labels = c(mc = "Monte Carlo CIs")
+    #   ) +
+    #   scale_y_continuous(
+    #     expand = c(0, 0),
+    #     limits = c(plot_data$min_activation, plot_data$max_activation)
+    #   ) +
+    #   guides(alpha = "none", color = guide_legend(order = 1), shape = guide_legend(order = 2)) +
+    #   expand_limits(y = 0) +
+    #   labs(
+    #     x = "Inference Value",
+    #     y = "Concept (Node)"
+    #   )
   } else if (fcmconfr_object$fcm_class == "ivfn") {
     ggplot() +
       geom_col(data = plot_data$aggregate_inferences, aes(x = .data$name, y = .data$crisp))
